612c71759a7fb9e0b5f4a6935e2e90e367ced87a
autodocs <autodocs>
2020-04-10 13:13:37 +0000

    build based on 120db6c

---
+++ dev/search_index.js
@@ -0,0 +1,75 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#RedPitayaDAQServer-1",
+    "page": "Home",
+    "title": "RedPitayaDAQServer",
+    "category": "section",
+    "text": "Julia package for the reconstruction of magnetic particle imaging (MPI) data"
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Home",
+    "title": "Introduction",
+    "category": "section",
+    "text": "This project provides functions for the reconstruction of MPI data. The project is implemented in the programming language Julia and contains algorithms for"
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Start julia and open the package mode by entering ]. Then enteradd MPIReco This will install the packages MPIReco.jl and all its dependencies. In particular this will install the core dependencies MPIFiles and RegularizedLeastSquares."
+},
+
+{
+    "location": "index.html#License-/-Terms-of-Usage-1",
+    "page": "Home",
+    "title": "License / Terms of Usage",
+    "category": "section",
+    "text": "The source code of this project is licensed under the MIT license. This implies that you are free to use, share, and adapt it. However, please give appropriate credit by citing the project."
+},
+
+{
+    "location": "index.html#Contact-1",
+    "page": "Home",
+    "title": "Contact",
+    "category": "section",
+    "text": "If you have problems using the software, find mistakes, or have general questions please use the issue tracker to contact us."
+},
+
+{
+    "location": "index.html#Contributors-1",
+    "page": "Home",
+    "title": "Contributors",
+    "category": "section",
+    "text": "Tobias Knopp\nMartin Möddel\nPatryk Szwargulski"
+},
+
+{
+    "location": "installation.html#",
+    "page": "Installation",
+    "title": "Installation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "installation.html#Getting-Started-1",
+    "page": "Installation",
+    "title": "Getting Started",
+    "category": "section",
+    "text": "In order to get started we will first gather some MPI data. To this end we enter the Pkg mode in Julia (]) and execute the unit tests of MPIRecotest MPIReco"
+},
+
+]}
