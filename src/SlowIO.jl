016b49b164919751f89752afa24d7bcc4f86bd6e
Tobias Knopp <tobias@knoppweb.de>
2020-09-04 22:29:34 +0200

    DIO interface working

--- src/SlowIO.jl
+++ src/SlowIO.jl
@@ -1,4 +1,4 @@
-export setSlowDAC, getSlowADC, slowDACClockDivider, DIO
+export setSlowDAC, getSlowADC, slowDACClockDivider, DIO, DIODirection
 
 function setSlowDAC(rp::RedPitaya, channel, value)
   command = string("RP:PDM:CHannel", Int64(channel), ":NextValueVolt ", value)
@@ -17,18 +17,38 @@ function slowDACClockDivider(rp::RedPitaya, value)
   send(rp, command)
 end
 
+
+function DIODirection(rp::RedPitaya, pin::Int, val::String)
+  if pin < 1 && pin > 8
+    error("RP has only 8 digital pins!")
+  end
+
+  if val != "IN" && val != "OUT"
+    error("value needs to be IN or OUT!")
+  end
+
+  send(rp, string("RP:DIO:DIR:PIN$(pin-1) ", val))
+  return
+end
+
 function DIO(rp::RedPitaya, pin::Int, val::Bool)
   if pin < 1 && pin > 8
     error("RP has only 8 digital pins!")
   end
 
+  DIODirection(rp, pin, "OUT")
+
   valStr = val ? "ON" : "OFF"
   send(rp, string("RP:DIO:PIN$(pin-1) ", valStr))
+  return
 end
 
 function DIO(rp::RedPitaya, pin::Int)
   if pin < 1 && pin > 8
     error("RP has only 8 digital pins!")
   end
+
+  DIODirection(rp, pin, "IN")
+
   return occursin("ON", query(rp,"RP:DIO:PIN$(pin-1)?"))
 end
